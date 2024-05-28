175e53672cb4bb4d3a248780b9a70d992a2ff127
zlept55 <rochinhas.pais@gmail.com>
2024-05-28 18:41:50 +0100

    figure changes

--- app/Livewire/History/Figure.php
+++ app/Livewire/History/Figure.php
@@ -6,8 +6,20 @@ use Livewire\Component;
 
 class Figure extends Component
 {
+    public $sub_page = 'history.concept';
+
     public function render()
     {
         return view('livewire.history.figure');
+    }
+
+    public function showSubPage($sub_page)
+    {
+        $this->sub_page = 'history.' . $sub_page;
+    }
+
+    public function goBack()
+    {
+        $this->sub_page = null;
     }
 }
